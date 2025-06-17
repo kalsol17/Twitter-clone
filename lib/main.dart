1cdbdc3cf6adde189edf353bdcdae00a5365f7bb
kaleab solomon <kaleab24gebreselassie@gmail.com>
2025-06-17 15:03:59 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFDE2606)),
+      ),
+      home:  MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+       GlobalKey<FormState> _signInKey=GlobalKey();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController=TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.blueAccent,
+        centerTitle: true,
+        title: Text('Twitter Clone'),
+      ),
+      body: Form(
+        key: _signInKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(hintText: "Enter an Email"),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(hintText: 'Enter a Password'),
+            ),
+            ElevatedButton(onPressed: (){
+              debugPrint('Email:${_emailController.text}');
+              debugPrint('Password:${_passwordController.text}');
+            },
+             child: Text('Submit'))
+          ],
+        ),
+      ),
+    );
+  }
+}
